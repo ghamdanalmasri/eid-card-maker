0318a56cd2cf70fb76a46ef38fc8b0f71ce3f798
ghamdanalmasri <144724107+ghamdanalmasri@users.noreply.github.com>
2026-05-21 22:00:24 +0300

    Create Templates.js

---
+++ components/Templates.js
@@ -0,0 +1,21 @@
+export const templates=[
+
+{
+id:1,
+title:"أخضر ذهبي",
+bg:"linear-gradient(135deg,#022c43,#0f766e)"
+},
+
+{
+id:2,
+title:"ذهبي فاخر",
+bg:"linear-gradient(135deg,#78350f,#f59e0b)"
+},
+
+{
+id:3,
+title:"أزرق ملكي",
+bg:"linear-gradient(135deg,#1e3a8a,#4f46e5)"
+}
+
+];
