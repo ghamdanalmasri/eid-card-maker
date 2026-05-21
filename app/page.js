d77d10829c30d21a3f3151126d39b031949bcf2f
ghamdanalmasri <144724107+ghamdanalmasri@users.noreply.github.com>
2026-05-21 18:37:57 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
 export default function Home() {
- return <h1>مرحبا</h1>
+  return (
+    <main style={{padding:20}}>
+      <h1>منصة بطاقات عيد الأضحى</h1>
+      <p>النسخة الأولية تعمل بنجاح</p>
+
+      <input placeholder="اكتب التهنئة" />
+      <br /><br />
+
+      <input placeholder="الاسم" />
+    </main>
+  );
 }
