0dcfa2264472b4781846c0e1e49304b8092029fe
ghamdanalmasri <144724107+ghamdanalmasri@users.noreply.github.com>
2026-05-21 22:27:56 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,23 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ar">
-      <body>{children}</body>
-    </html>
-  );
+import "./globals.css";
+
+export const metadata={
+title:"منصة بطاقات العيد"
+};
+
+export default function RootLayout({children}){
+
+return(
+
+<html lang="ar">
+
+<body>
+
+{children}
+
+</body>
+
+</html>
+
+)
+
 }
